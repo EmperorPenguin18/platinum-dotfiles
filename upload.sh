f9d1ffe7958d3f83dce12c465ca9444b7a9e84ce
EmperorPenguin18 <60635017+EmperorPenguin18@users.noreply.github.com>
2020-08-19 19:30:25 -0400

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,1 +1,1 @@
-/usr/bin/rclone move -P /mnt/Local encrypted: --exclude-from *partial~ --delete-empty-src-dirs --min-age 1d
+/usr/bin/timeout -k 5 6h /usr/bin/rclone move --config /mnt/rclone.conf -P /mnt/Local encrypted: --exclude *partial~ --delete-empty-src-dirs --min-age 1d
